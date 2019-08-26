ecb5880688993d809b1259aaa4ff372ddd9860d7
mbao1996 <mbao1996@hotmail.com>
2019-08-26 16:46:29 +0800

    merge more sheets

--- xlmerge.py
+++ xlmerge.py
@@ -4,13 +4,12 @@ import os, time
 from pandas.core.dtypes.inference import is_number
 
 fn_config = "c:\\PythonWork\\test\\config.xlsx"
-work_catalog = "c:\\PythonWork\\test\\"
-fn = work_catalog + "demo.xlsx"
-sheet = 'Sheet1'
-opr = []
-
-row_start = 3
-col_opr = 7
+#work_catalog = "c:\\PythonWork\\test\\"
+#fn = work_catalog + "demo.xlsx"
+#sheet = 'Sheet1'
+#opr = []
+#row_start = 3
+#col_opr = 7
 #col_chg = [8,9,10]
 
 def is_number(variate):
@@ -48,7 +47,7 @@ def save_fn(wb, fn):
         os._exit(0)
 def read_config(fn):
     wb = open_fn(fn, False)
-    ws = wb[sheet]
+    ws = wb['Sheet1']
     rt = []
     col_chg = []
     if( ws.cell(1, 1).value == u"工作目录" ):
@@ -88,56 +87,63 @@ def read_config(fn):
     rt.append(opr)
     rt.append(col_chg)
     return(rt)
+def merge_sheet(wb, sheet, cfg):
+    ws = wb[sheet]
+    fn = cfg[0] + cfg[1]
+    row_start = cfg[2]
+    col_opr = cfg[3]
+    col_chg = cfg[4]
+    opr = []
+    for i in range(row_start, ws.max_row+1):
+        op = ws.cell(i, col_opr).value          # get opr
+        op = ''.join(op.split())
+        add_str_to_list(opr, op)
+    for k in range(len(opr)):   # every files to be merged
+        print('-----------', opr[k])
+        fn_div = fn.split('.')
+        fn_rd = fn_div[0] + '_' + opr[k] + '.' + fn_div[1]
+    
+        wb_rd = open_fn(fn_rd, False)
+        sheets_rd = wb_rd.sheetnames
+        if sheet not in sheets_rd:
+            print('sheet:', sheet, ' is not exist in file:', fn_rd)
+            return(wb)
+        ws_rd = wb_rd[sheet]
+        rd = None
+        rd_sn = 0
+        wr = None
+        wr_sn = 0
+        for i in range(row_start, ws_rd.max_row+1):     # 副文件的每一行
+            if( ws_rd.cell(i, 1).value != None ):
+                rd = ws_rd.cell(i, 1).value
+                rd_sn = 0
+            else:
+                rd_sn += 1
+            if( ws_rd.cell(i, col_opr).value == opr[k] ):   # 确认副文件的操作者
+                for j in range(row_start, ws.max_row+1):    # 主文件中搜索
+                    if( ws.cell(j, 1).value != None ):
+                        wr = ws.cell(j, 1).value
+                        wr_sn = 0
+                    else:
+                        wr_sn += 1
+                    if( ws.cell(j, col_opr).value == opr[k] ):   # 主文件中确认副文件的操作者
+                        if( wr == rd and wr_sn == rd_sn ):      # 确认是同一行
+                            for col in range(len(col_chg)):
+                                ws.cell(j, col_chg[col]).value = ws_rd.cell(i, col_chg[col]).value
+                            break
+    return(wb)
 
 cfg = read_config(fn_config)
-work_catalog = cfg[0]
-fn = work_catalog + cfg[1]
-row_start = cfg[2]
-col_opr = cfg[3]
-col_chg = cfg[4]
+fn = cfg[0] + cfg[1]
+wb = open_fn(fn, False)
+sheets = wb.sheetnames
+for i in range(len(sheets)):
+    print(sheets[i])
+    wb = merge_sheet(wb, sheets[i], cfg)
 
-wb = open_fn(fn, True)
-ws = wb[sheet]
-# get opr
-for i in range(row_start, ws.max_row+1):
-    op = ws.cell(i, col_opr).value
-    op = ''.join(op.split())
-    add_str_to_list(opr, op)
-for k in range(len(opr)):   # every files to be merged
-    print('-----------', opr[k])
-    fn_div = fn.split('.')
-    fn_rd = fn_div[0] + '_' + opr[k] + '.' + fn_div[1]
-
-    wb_rd = open_fn(fn_rd, False)
-    ws_rd = wb_rd[sheet]
-    rd = None
-    rd_sn = 0
-    wr = None
-    wr_sn = 0
-    for i in range(row_start, ws_rd.max_row+1):     # 副文件的每一行
-        if( ws_rd.cell(i, 1).value != None ):
-            rd = ws_rd.cell(i, 1).value
-            rd_sn = 0
-        else:
-            rd_sn += 1
-        if( ws_rd.cell(i, col_opr).value == opr[k] ):   # 确认副文件的操作者
-            for j in range(row_start, ws.max_row+1):    # 主文件中搜索
-                if( ws.cell(j, 1).value != None ):
-                    wr = ws.cell(j, 1).value
-                    wr_sn = 0
-                else:
-                    wr_sn += 1
-                if( ws.cell(j, col_opr).value == opr[k] ):   # 主文件中确认副文件的操作者
-                    if( wr == rd and wr_sn == rd_sn ):      # 确认是同一行
-                        for col in range(len(col_chg)):
-                            ws.cell(j, col_chg[col]).value = ws_rd.cell(i, col_chg[col]).value
-                        break
-
-#save_fn(wb, fn)
-#print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 time_flag = time.strftime("%Y%m%d_%H%M%S", time.localtime())
 fn_div = fn.split('.')
-fn_wr = fn_div[0] + '_' + time_flag + '.xlsm'
+fn_wr = fn_div[0] + '_' + time_flag + '.xlsx'
 save_fn(wb, fn_wr)
 
 print("work done")
